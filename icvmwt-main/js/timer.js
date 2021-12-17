e52a782363c635f6085491b61e2d30ac1bb8ea9e
Yuvraj <tyuvraj72@gmail.com>
2021-12-18 02:23:32 +0530

    per-final upload

--- icvmwt-main/js/timer.js
+++ icvmwt-main/js/timer.js
@@ -1,31 +1,40 @@
-var target_date = new Date("March 20, 2021 08:37:25").getTime() // set the countdown date
-var days, hours, minutes, seconds; // variables for time units
+(function () {
+	const second = 1000,
+		minute = second * 60,
+		hour = minute * 60,
+		day = hour * 24;
 
-var countdown = document.getElementById("tiles"); // get tag element
+	//I'm adding this section so I don't have to keep updating this pen every year :-)
+	//remove this if you don't need it
+	let today = new Date(),
+		dd = String(today.getDate()).padStart(2, "0"),
+		mm = String(today.getMonth() + 1).padStart(2, "0"),
+		yyyy = today.getFullYear(),
+		nextYear = yyyy + 1,
+		// I added the date here you can change it to whatever you want
+		dayMonth = "04/21/",
+		birthday = dayMonth + yyyy;
 
-getCountdown();
+	today = mm + "/" + dd + "/" + yyyy;
+	if (today > birthday) {
+		birthday = dayMonth + nextYear;
+	}
+	//end
 
-setInterval(function () { getCountdown(); }, 1000);
+	const countDown = new Date(birthday).getTime(),
+		x = setInterval(function () {
+			const now = new Date().getTime(),
+				distance = countDown - now;
 
-function getCountdown(){
+			(document.getElementById("days").innerText = Math.floor(distance / day)), (document.getElementById("hours").innerText = Math.floor((distance % day) / hour)), (document.getElementById("minutes").innerText = Math.floor((distance % hour) / minute)), (document.getElementById("seconds").innerText = Math.floor((distance % minute) / second));
 
-	// find the amount of "seconds" between now and target
-	var current_date = new Date().getTime();
-	var seconds_left = (target_date - current_date) / 1000;
-
-	days = pad( parseInt(seconds_left / 86400) );
-	seconds_left = seconds_left % 86400;
-		 
-	hours = pad( parseInt(seconds_left / 3600) );
-	seconds_left = seconds_left % 3600;
-		  
-	minutes = pad( parseInt(seconds_left / 60) );
-	seconds = pad( parseInt( seconds_left % 60 ) );
-
-	// format countdown string + set tag value
-	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
-}
-
-function pad(n) {
-	return (n < 10 ? '0' : '') + n;
-}
+			//do something later when date is reached
+			if (distance < 0) {
+				document.getElementById("headline").innerText = "It's my birthday!";
+				document.getElementById("countdown").style.display = "none";
+				document.getElementById("content").style.display = "block";
+				clearInterval(x);
+			}
+			//seconds
+		}, 0);
+})();
